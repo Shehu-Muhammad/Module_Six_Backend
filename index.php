3105862973f05f828544d236a55b4429781b25b8
Student <student@StudentiMac-33.local>
2022-08-15 16:35:02 -0400

    added code to display some data from the database, but it needs to be changed so that when another movie is added to the database, it shows all the details for the new movie. Also, I need to add a user id column to the details table so that the edit and delete only shows on movies that user added

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+include_once 'includes/dbMovies.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,5 +20,98 @@
             </li>
         </ul>
     </nav>
+<?php
+        $sql = "SELECT * from movies";
+        $sql2 = "SELECT * from details";
+        $sql3 = "SELECT * from genre";
+        $sql4 = "SELECT * from ratings";
+        $allMovies = mysqli_query($conn, $sql);
+        $allDetails = mysqli_query($conn, $sql2);
+        $allGenres = mysqli_query($conn, $sql3);
+        $allRatings = mysqli_query($conn, $sql4);
+
+        function getTitle($allMovies) {
+            while ($currentMovie = mysqli_fetch_assoc( $allMovies )) {
+                $title = $currentMovie['title'];
+                echo("<li>".$title."</li>");
+            }
+        }
+
+        function getDetails($allDetails) {
+            while ($currentDetails = mysqli_fetch_assoc($allDetails)) {
+                $hours = intDiv($currentDetails['run_time'], 3600);
+                $minutes = $currentDetails['run_time']/60%60;
+                $releaseYear = $currentDetails['release_year'];
+                echo("<br> hours: ".$hours."<br> minutes: ".$minutes."<br> release-year: ".$releaseYear);
+                // while( $currentRating = mysqli_fetch_assoc($allRatings) ) {
+                //     if($allDetails['rating_id'] == $currentRating['id']) {
+                //         $rating = $currentRating['rating'];
+                //         echo("<br> Rating: ".$rating);
+                //     }
+                // }
+            }
+        }
+        ?>
+
+        <?php
+        // exit();
+        while ($currentMovie = mysqli_fetch_assoc( $allMovies )) {
+?>
+        <ol>
+           <li>
+<?php
+                $title = $currentMovie['title'];
+                $allDetails = mysqli_fetch_assoc($allDetails);
+                $hours = intDiv($allDetails['run_time'], 3600);
+                $minutes = $allDetails['run_time']/60%60;
+                $releaseYear = $allDetails['release_year'];
+                while( $currentRating = mysqli_fetch_assoc($allRatings) ) {
+                    if($allDetails['rating_id'] == $currentRating['id']) {
+                        $rating = $currentRating['rating'];
+                    }
+                }
+?>
+                Title: <?php echo ($title); ?><br>
+                Hour(s): <?php echo ($hours); ?><br>
+                Minute(s): <?php echo ($minutes); ?><br>
+                Release Year: <?php echo ($releaseYear); ?><br>
+                Rating: <?php echo($rating); ?><br>
+                Genre:
+<?php 
+                $genre_Ids = $allDetails['genre_ids'];
+                $genre_Ids = explode(",", $genre_Ids);
+                $genre_Ids_string = "";
+                for( $currentGenreIdIndex = 0; $currentGenreIdIndex < count( $genre_Ids ) ; $currentGenreIdIndex++  ) {
+                    if( $currentGenreIdIndex == count( $genre_Ids ) - 2 ) { 
+                        $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ];
+                    } else if(trim($genre_Ids[$currentGenreIdIndex]) == "") {
+                        continue;
+                    } else {
+                        $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ] . ", ";
+                    }
+                }
+                $sqlGenres = "SELECT * from genre where id in";
+                $sqlGenres .= " (";
+                $sqlGenres .= "$genre_Ids_string";
+                $sqlGenres .= ")";
+                $sqlGetGenresList = mysqli_query($conn, $sqlGenres);
+                while($result = mysqli_fetch_assoc($sqlGetGenresList)) {
+                    echo(ucfirst($result['name']." "));
+                }
+?>
+
+                <br>
+               <a href="<?php echo("index.php?movieEditId=" . urlencode( $currentMovie['id'] )); ?>">Edit</a> 
+               <a href="<?php echo("index.php?movieDeleteId=" . urlencode( $currentMovie['id'] )); ?>">Delete</a>
+            </li>
+        </ol>
+<?php
+        }
+?>
+<?php
+        if( isset( $_GET['movieEditId'] )) {
+
+        }
+?>
 </body>
 </html>
