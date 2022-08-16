7c7573cf525103b78105671c2334f88f7e5ce9f5
Student <student@StudentiMac-33.local>
2022-08-16 11:48:32 -0400

    coded index.php to dynamically show movie data once the movie was added. Moved first attempt to php file called index2.php

--- index.php
+++ index.php
@@ -21,97 +21,111 @@ include_once 'includes/dbMovies.inc.php';
         </ul>
     </nav>
 <?php
-        $sql = "SELECT * from movies";
-        $sql2 = "SELECT * from details";
-        $sql3 = "SELECT * from genre";
-        $sql4 = "SELECT * from ratings";
-        $allMovies = mysqli_query($conn, $sql);
-        $allDetails = mysqli_query($conn, $sql2);
-        $allGenres = mysqli_query($conn, $sql3);
-        $allRatings = mysqli_query($conn, $sql4);
+    // Query - SQL statement
+    // $sql = "SELECT * FROM ";
+    // mysqli_query($conn, $sql);
+    // mysqli_fetch_assoc( $ )
+    $sql = "SELECT * FROM details";
+    // echo($sql);
+    $query = mysqli_query($conn, $sql);
+    // echo("<pre>");
+    // var_dump($query);
+    // echo("</pre>");
+    echo("<ol>");
+    while($currentData = mysqli_fetch_assoc($query)) {
+        // print_r ($currentData);
+        echo("<li>");
+        $id = $currentData['id'];
+        $titleId = $currentData['movie_id'];
+        $genreIds = $currentData['genre_ids'];
+        $ratingId = $currentData['rating_id'];
+        $releaseYear = $currentData['release_year'];
+        $seconds = $currentData['run_time'];
 
-        function getTitle($allMovies) {
-            while ($currentMovie = mysqli_fetch_assoc( $allMovies )) {
+
+        $movieSQL = "SELECT * FROM movies";
+        $movieQuery = mysqli_query($conn, $movieSQL);
+        while($currentMovie = mysqli_fetch_assoc($movieQuery)) {
+            if( $currentMovie['id'] == $titleId ) {
                 $title = $currentMovie['title'];
-                echo("<li>".$title."</li>");
             }
         }
 
-        function getDetails($allDetails) {
-            while ($currentDetails = mysqli_fetch_assoc($allDetails)) {
-                $hours = intDiv($currentDetails['run_time'], 3600);
-                $minutes = $currentDetails['run_time']/60%60;
-                $releaseYear = $currentDetails['release_year'];
-                echo("<br> hours: ".$hours."<br> minutes: ".$minutes."<br> release-year: ".$releaseYear);
-                // while( $currentRating = mysqli_fetch_assoc($allRatings) ) {
-                //     if($allDetails['rating_id'] == $currentRating['id']) {
-                //         $rating = $currentRating['rating'];
-                //         echo("<br> Rating: ".$rating);
-                //     }
-                // }
+        $ratingSQL = "SELECT * FROM ratings";
+        $ratingQuery = mysqli_query($conn, $ratingSQL);
+        while($currentRating = mysqli_fetch_assoc($ratingQuery)) {
+            if( $currentRating['id'] == $ratingId ) {
+                $rating = $currentRating['rating'];
             }
         }
-        ?>
 
-        <?php
+        // $genre_Ids = $allDetails['genre_ids'];
+        $genre_Ids = explode(",", $genreIds);
+        $genre_Ids_string = "";
+        for( $currentGenreIdIndex = 0; $currentGenreIdIndex < count( $genre_Ids ) ; $currentGenreIdIndex++  ) {
+            if( $currentGenreIdIndex == count( $genre_Ids ) - 2 ) { 
+                $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ];
+            } else if(trim($genre_Ids[$currentGenreIdIndex]) == "") {
+                continue;
+            } else {
+                $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ] . ", ";
+            }
+        }
+        // echo($genre_Ids_string);
         // exit();
-        while ($currentMovie = mysqli_fetch_assoc( $allMovies )) {
-?>
-        <ol>
-           <li>
-<?php
-                $title = $currentMovie['title'];
-                $allDetails = mysqli_fetch_assoc($allDetails);
-                $hours = intDiv($allDetails['run_time'], 3600);
-                $minutes = $allDetails['run_time']/60%60;
-                $releaseYear = $allDetails['release_year'];
-                while( $currentRating = mysqli_fetch_assoc($allRatings) ) {
-                    if($allDetails['rating_id'] == $currentRating['id']) {
-                        $rating = $currentRating['rating'];
-                    }
-                }
-?>
-                Title: <?php echo ($title); ?><br>
-                Hour(s): <?php echo ($hours); ?><br>
-                Minute(s): <?php echo ($minutes); ?><br>
-                Release Year: <?php echo ($releaseYear); ?><br>
-                Rating: <?php echo($rating); ?><br>
-                Genre:
-<?php 
-                $genre_Ids = $allDetails['genre_ids'];
-                $genre_Ids = explode(",", $genre_Ids);
-                $genre_Ids_string = "";
-                for( $currentGenreIdIndex = 0; $currentGenreIdIndex < count( $genre_Ids ) ; $currentGenreIdIndex++  ) {
-                    if( $currentGenreIdIndex == count( $genre_Ids ) - 2 ) { 
-                        $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ];
-                    } else if(trim($genre_Ids[$currentGenreIdIndex]) == "") {
-                        continue;
-                    } else {
-                        $genre_Ids_string .= $genre_Ids[ $currentGenreIdIndex ] . ", ";
-                    }
-                }
-                $sqlGenres = "SELECT * from genre where id in";
-                $sqlGenres .= " (";
-                $sqlGenres .= "$genre_Ids_string";
-                $sqlGenres .= ")";
-                $sqlGetGenresList = mysqli_query($conn, $sqlGenres);
-                while($result = mysqli_fetch_assoc($sqlGetGenresList)) {
-                    echo(ucfirst($result['name']." "));
-                }
-?>
 
-                <br>
-               <a href="<?php echo("index.php?movieEditId=" . urlencode( $currentMovie['id'] )); ?>">Edit</a> 
-               <a href="<?php echo("index.php?movieDeleteId=" . urlencode( $currentMovie['id'] )); ?>">Delete</a>
-            </li>
-        </ol>
-<?php
+        $sqlGenres = "SELECT * from genre where id in";
+        $sqlGenres .= " (";
+        $sqlGenres .= "$genre_Ids_string";
+        $sqlGenres .= ")";
+        $sqlGetGenresList = mysqli_query($conn, $sqlGenres);
+        $genres = "";
+        while($result = mysqli_fetch_assoc($sqlGetGenresList)) {
+            // echo(ucfirst($result['name']." "));
+            $genres .= ucfirst($result['name']." ");
         }
-?>
-<?php
-        if( isset( $_GET['movieEditId'] )) {
+        // echo $genres;
+        // exit();
 
-        }
+        echo("Title: ".$title);
+        echo ("<br>");
+        echo("Hours: ".intDiv($seconds, 3600));
+        echo ("<br>");
+        echo("Minutes: ".$seconds/60%60);
+        echo ("<br>");
+        echo("Release Year: ".$releaseYear);
+        echo ("<br>");
+        echo("Rating: ".$rating);
+        echo ("<br>");
+        echo("Genre(s): ".$genres);
+        echo("</li>");
+        echo ("<br>");
+    }
+    echo("</ol>");
+    exit();
 ?>
+<?php
+// Hard coded for now to not get errors to undefined variables
+            $title = "Dog";
+            $hours = 1;
+            $minutes = 41;
+            $releaseYear = 2022;
+            $rating = "PG-13";
+            $genres = "Comedy Drama";
+?>
+    <ol>
+        <li>
+            Title: <?php echo ($title); ?><br>
+            Hour(s): <?php echo ($hours); ?><br>
+            Minute(s): <?php echo ($minutes); ?><br>
+            Release Year: <?php echo ($releaseYear); ?><br>
+            Rating: <?php echo($rating); ?><br>
+            Genre: <?php echo ($genres); ?><br>
+            <a href="<?php echo("index.php?movieEditId=" . urlencode( $currentMovie['id'] )); ?>">Edit</a> 
+            <a href="<?php echo("index.php?movieDeleteId=" . urlencode( $currentMovie['id'] )); ?>">Delete</a>
+        </li>
+    </ol>
+
+                
 </body>
 </html>
